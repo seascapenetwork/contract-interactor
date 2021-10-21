2771a8779e50ec986c99b00eeab864c5c64f9732
DESKTOP-IVRHEJ7\JiZai <719896508@qq.com>
2021-10-21 12:23:58 +0800

    add db heartbeat and add execLeaderboard console with date

--- db.js
+++ db.js
@@ -1,14 +1,58 @@
 let mysql = require('mysql');
 
+var con = null;
+
 module.exports.getConnection = async function() {
-	let con = mysql.createConnection({
+	if (!con){
+		connect().catch(console.log);
+	}
+	return con;
+};
+
+let connect = async () => {
+	if (con !== null){
+		con.destroy();
+		con = null;
+	}
+	con = mysql.createConnection({
 		host: process.env.DATABASE_HOST,
 		user: process.env.DATABASE_USERNAME,
 		password: process.env.DATABASE_PASSWORD,
 		database: process.env.DATABASE_NAME
 	});
 
+	con.connect(handleError);
+	// con.on('error', handleError);
+
+	// run sql keep conn live
+	setInterval(() => {
+		// con.ping(err => {
+		// 	if (err) {
+		// 		console.log("ping error:", err)
+		// 	}
+		// })
+		new Promise(function(resolve, reject) {
+			con.query("select version()", function(err, res, _fields) {
+				if (err) {
+					console.log('db err:',err);
+				} else {
+					// console.log(res[0]["version()"]);
+				}
+			});
+		});
+	}, 1000);
 	let res = await con.connect();
 
 	return con;
 };
+
+function handleError (err) {
+	if (err) {
+		// 如果是连接断开，自动重新连接
+		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+			connect().then()
+		} else {
+			console.error(err.stack || err);
+		}
+	}
+}
--- index.js
+++ index.js
@@ -3,9 +3,9 @@ let dailyLeaderboard = require('./daily_leaderboard');
 let allTimeLeaderboard = require('./alltime_leaderboard');
 const schedule = require('node-schedule');
 // or server to listen to sign up
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 // account
 let admin = blockchain.addAccount(process.env.ACCOUNT_1);
@@ -14,6 +14,17 @@ let burningAdmin = blockchain.addAccount(process.env.NFTBURNING_DEPLOYER);
 
 let crowns;
 let nftRush;
+let getDate = function () {
+	let date = new Date((new Date()).getTime()),
+		Y = date.getFullYear() + '/',
+		M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '/',
+		D = (date.getDate() < 10 ? '0'+date.getDate() : date.getDate()) + ' ',
+		h = (date.getHours() < 10 ? '0'+date.getHours() : date.getHours()) + ':',
+		m = (date.getMinutes() < 10 ? '0'+date.getMinutes() : date.getMinutes()) + ':',
+		s = (date.getSeconds() < 10 ? '0'+date.getSeconds() : date.getSeconds());
+	return Y+M+D+h+m+s;
+};
+
 let getCrowns = async function () {
 	if (!crowns) {
 		crowns = await blockchain.loadCrowns();
@@ -29,6 +40,7 @@ let getNftRush = async function () {
 };
 
 let execDailyLeaderboard = async function () {
+	console.log(getDate(),"====================================== execDailyLeaderboard start ===========================================");
 	let data = await dailyLeaderboard.calculateDailyWinners();
 	if (!data) {
 		return;
@@ -36,17 +48,15 @@ let execDailyLeaderboard = async function () {
 
 	let approvement = await approveCrowns(data, 'daily');
 	if (approvement === false) {
-		console.log("Failed to approve required amount of CWS. Not enough balance");
+		console.log(getDate(),": Failed to approve required amount of CWS. Not enough balance");
 		return;
 	}
 
 	let nftRush = await getNftRush();
 
-	console.log(data);
-
-
+	console.log(getDate(),"-------------------------------- setDailyLeaderboard ------------------------------------");
 	let txid = await dailyLeaderboard.setDailyLeaderboard(nftRush, data, admin);
-	console.log("Daily leaderboard winners announnced! Txid: ", txid, " for ", admin.address);
+	console.log(getDate(),": Daily leaderboard winners announnced! Txid: ", txid, " for ", admin.address);
 };
 
 let execAllTimeLeaderboard = async function () {
@@ -57,7 +67,7 @@ let execAllTimeLeaderboard = async function () {
 
 	let approvement = await approveCrowns(data, 'alltime');
 	if (approvement === false) {
-		console.log("Failed to approve required amount of CWS. Not enough balance");
+		console.log(getDate(),": Failed to approve required amount of CWS. Not enough balance");
 		return;
 	}
 
@@ -67,7 +77,7 @@ let execAllTimeLeaderboard = async function () {
 
 
 	let txid = await allTimeLeaderboard.setAllTimeLeaderboard(nftRush, data, admin);
-	console.log("All Time leaderboard winners announnced! Txid: ", txid, " for ", admin.address);
+	console.log(getDate(),": All Time leaderboard winners announnced! Txid: ", txid, " for ", admin.address);
 };
 
 let calculateTotalPrize = function (data, type) {
@@ -100,6 +110,7 @@ let approveCrowns = async function (data, type) {
 	// for now all type of leaderboard reward players with the same amount of
 	// CWS tokens.
 	// in the next versions, approving CWS tokens should be in the leaderboard blocks
+	console.log(getDate(),"====================================== approveCrowns start ===========================================");
 	var totalPrize = blockchain.web3.utils.toWei(calculateTotalPrize(data, type).toString());
 
 	if (totalPrize <= 0) {
@@ -122,11 +133,11 @@ let approveCrowns = async function (data, type) {
 		return false;
 	}
 
-
+	console.log(getDate(),"----------------------- approve ------------------------");
 	try {
 		await crowns.methods.approve(nftRush._address, totalPrize).send({ from: admin.address, gasPrice: gasPrice, gas: approveGasEstimate * 3 });
 	} catch (e) {
-		console.error(e);
+		console.log(getDate()," approve err:",e);
 		return false;
 	}
 
@@ -284,6 +295,7 @@ app.get('/sign-scape-forum-quality', async function (req, res) {
 
 	res.send(signature);
 })
+
 
 app.listen(port, () => {
     //schedule.scheduleJob('0 * * * * *', execDailyLeaderboard);
