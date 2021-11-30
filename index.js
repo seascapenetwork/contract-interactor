de98c50edbe0090de0f2554a3038974e6420314a
DESKTOP-IVRHEJ7\JiZai <719896508@qq.com>
2021-11-30 21:53:25 +0800

    print rib price api

--- index.js
+++ index.js
@@ -294,7 +294,7 @@ app.get('/rib/price', async function(req, res) {
 	const RIB = new seadex.Token(
 		seadex.ChainId.MOONRIVER,
 		process.env.RIB_ADDRESS,
-		18
+		9
 	);
 
 	let rib_price;
@@ -302,12 +302,9 @@ app.get('/rib/price', async function(req, res) {
 		const pair = await seadex.Fetcher.fetchPairData(RIB, seadex.WMOVR[RIB.chainId], provider);
 		const route = new seadex.Route([pair], RIB);
 		rib_price = route.midPrice.toSignificant(6);
-		console.log('=================================== /rib/price =========================================');
-		console.log({RIB: RIB, pair: pair, route: route, rib_price: rib_price});
 	} catch(e) {
 		rib_price = 0;
 	}
-	console.log(rib_price);
 	res.send(rib_price);
 })
 
