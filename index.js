aed3e6f358d14457d38a48cfdb1803bda6e2cae8
DESKTOP-IVRHEJ7\JiZai <719896508@qq.com>
2021-12-01 21:06:50 +0800

    x

--- index.js
+++ index.js
@@ -301,7 +301,7 @@ app.get('/rib/price', async function(req, res) {
 	// try {
 	// 	const pair = await seadex.Fetcher.fetchPairData(RIB, seadex.WMOVR[RIB.chainId], provider).catch(console.error);
 	// 	const route = new seadex.Route([pair], RIB);
-		rib_price = route.midPrice.toSignificant(6);
+	// 	rib_price = route.midPrice.toSignificant(6);
 	// } catch(e) {
 		rib_price = '0.0025';
 	// }
